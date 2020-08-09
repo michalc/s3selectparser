c20d0e9869fb4d798cdab81170fed7535cf86cbe
Michal Charemza <michal@charemza.name>
2020-08-09 17:19:47 +0100

    feat: very basic projection parsing

--- s3selectparser.py
+++ s3selectparser.py
@@ -1,13 +1,22 @@
 from pyparsing import (
     CaselessKeyword,
+    Keyword,
+    Word,
+    alphas,
+    alphanums,
+    delimitedList,
 )
 
 SELECT, FROM = map(
     CaselessKeyword, 'select from'.split()
 )
+ASTERISK = Keyword('*')
+
+identifier = Word(alphas, alphanums + '_').setName('identifier')
+projections = (delimitedList(identifier) | ASTERISK).setResultsName('projections')
 
 s3_select_parser = \
     SELECT + \
-    '*' + \
+    projections + \
     FROM + \
     'S3Object'
--- test.py
+++ test.py
@@ -5,6 +5,14 @@ from s3selectparser import s3_select_parser
 
 class TestIntegration(unittest.TestCase):
 
-    def test_parser(self):
+    def test_select_all(self):
         parsed = s3_select_parser.parseString('SELECT * FROM S3Object')
-        self.assertEqual(list(parsed), ['select', '*', 'from', 'S3Object'])
+        self.assertEqual(parsed.asDict(), {
+            'projections': ['*']
+        })
+
+    def test_select_columns(self):
+        parsed = s3_select_parser.parseString('SELECT a, b, c FROM S3Object')
+        self.assertEqual(parsed.asDict(), {
+            'projections': ['a', 'b', 'c']
+        })
