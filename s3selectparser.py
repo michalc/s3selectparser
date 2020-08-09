95349cad382da535129fbe552018002a2702b015
Michal Charemza <michal@charemza.name>
2020-08-09 17:40:29 +0100

    feat: simple table name parsing

--- s3selectparser.py
+++ s3selectparser.py
@@ -1,5 +1,6 @@
 from pyparsing import (
     CaselessKeyword,
+    Group,
     Keyword,
     Word,
     alphas,
@@ -15,8 +16,11 @@ ASTERISK = Keyword('*')
 identifier = Word(alphas, alphanums + '_').setName('identifier')
 projections = (delimitedList(identifier) | ASTERISK).setResultsName('projections')
 
+identifier_all_rows = Group(identifier + '[*]')
+table_name = (identifier_all_rows | identifier).setResultsName('table_name')
+
 s3_select_parser = \
     SELECT + \
     projections + \
     FROM + \
-    'S3Object'
+    table_name
--- test.py
+++ test.py
@@ -8,11 +8,20 @@ class TestIntegration(unittest.TestCase):
     def test_select_all(self):
         parsed = s3_select_parser.parseString('SELECT * FROM S3Object')
         self.assertEqual(parsed.asDict(), {
-            'projections': ['*']
+            'projections': ['*'],
+            'table_name': ['S3Object'],
         })
 
     def test_select_columns(self):
         parsed = s3_select_parser.parseString('SELECT a, b, c FROM S3Object')
         self.assertEqual(parsed.asDict(), {
-            'projections': ['a', 'b', 'c']
+            'projections': ['a', 'b', 'c'],
+            'table_name': ['S3Object'],
+        })
+
+    def test_select_asterisk(self):
+        parsed = s3_select_parser.parseString('SELECT a, b, c FROM S3Object[*]')
+        self.assertEqual(parsed.asDict(), {
+            'projections': ['a', 'b', 'c'],
+            'table_name': [['S3Object', '[*]']],
         })
